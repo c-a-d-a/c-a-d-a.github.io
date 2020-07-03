4bf9100008ea0fc578791955eb4f63529fe031db
cada <ariesatienza25@gmail.com>
2020-07-04 07:20:19 +0800

    v2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "dc73ef7a4b8098848da22f3b994ca657",
 "/": "dc73ef7a4b8098848da22f3b994ca657",
-"main.dart.js": "c049721c84db94013d6bf236a9d22fe5",
+"main.dart.js": "56696c0b8a902a7e47f691fd48dd43bd",
 "manifest.json": "ef527010be97a21f75bd4c0aafa580e3"
 };
 
